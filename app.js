ba4f84ea9c66cb6a72284237a48aa7803a707003
Busayo <laminabusayo@gmail.com>
2023-06-22 20:13:56 +0100

    Lyrics-app

--- app.js
+++ app.js
@@ -5,43 +5,45 @@ const results = document.querySelector(".results");
 
 const Api = "https://api.lyrics.ovh";
 
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let searchValue = search.value;
+  let searchValue = search.value.trim();
 
   if (!searchValue) {
     alert("Nothing to search!!!!");
   } else {
     Search(searchValue);
   }
+
 });
 
 async function Search(searchValue) {
-  const s = await fetch(`${Api}/suggest/${searchValue}`);
-  const data = await s.json().then();
-
-  console.log(data);
-  displayData(data);
+  try {
+    const s = await fetch(`${Api}/suggest/${searchValue}`);
+    const data = await s.json().then();
+    displayData(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function displayData(data) {
   results.innerHTML = `
-
   <ul>
-  ${data.data.map(
-    (song) => `
-  <li>
-      <div>
-      <strong>${song.artist.name}</strong> - ${song.title}
-      </div>
-      <span data-artist=${song.artist.name}  data-songtitle=${song.title}>
-      Get Lyrics
-      </span>
-  </li>
-  
-  `
-  )}
+  ${data.data
+    .map(
+      (song) => `<li>
+                    <div>
+                    <strong>${song.artist.name}</strong> - ${song.title}
+                    </div>
+                    <span data-artist="${song.artist.name}"  data-songtitle="${song.title}">
+                    Get Lyrics
+                    </span>
+                 </li>`
+    )
+    .join("")}
   </ul>
   `;
 }
@@ -58,8 +60,11 @@ results.addEventListener("click", (e) => {
 });
 
 async function getLyrics(artist, songTitle) {
-  const res = await fetch(`${Api}/v1/${artist}/${songTitle}`);
-
-  const data = await res.json();
-  console.log(data);
+  try {
+    const res = await fetch(`${Api}/v1/${artist}/${songTitle}`);
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
 }
